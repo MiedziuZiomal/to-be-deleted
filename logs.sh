64d5d08f225ce26c778e024c123a5e486e52ba4e
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-10 10:05:01 +0200

    created logs function

---
+++ logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while [ -n "$1" ]
+do
+case "$1" in
+--logs) 
+    if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+        for i in $(seq 1 $2); do
+            echo "log$i.txt" > log$i.txt
+        done
+    else
+        for i in $(seq 1 100); do
+            echo "log$i.txt" > log$i.txt
+        done
+    fi
+;;
+*) echo "opcja $1 nieznana";;
+esac
+shift
+done
